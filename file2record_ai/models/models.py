44ff311408880280117b4a6b773af19b23dc4805
Michel Perrocheau <michel@scalizer.fr>
2024-06-03 11:33:30 +0200

    [IMP] remove test values

--- file2record_ai/models/models.py
+++ file2record_ai/models/models.py
@@ -15,9 +15,8 @@ class BaseModel(models.AbstractModel):
         if config_id.record_creation_method == 'ai':
             completion_id = config_id.ai_completion_id
             prompt = '%s \n %s' % (completion_id.get_prompt(), content)
-            # res = completion_id.create_completion(prompt=prompt)
-            # return json.loads(res[0])
-            return {'email_normalized': 'antoine@teremis.fr', 'message_bounce': 0, 'name': 'Antoine NEBOUT', 'display_name': 'Antoine NEBOUT', 'parent_id': {'name': False, 'phone': False, 'email': False, 'street': False, 'city': False, 'zip': False, 'country_id': {'name': False, 'code': False}, 'is_company': False, 'company_name': False}, 'ref': False, 'vat': False, 'company_registry': False, 'website': False, 'comment': False, 'active': False, 'employee': False, 'function': False, 'street': '16d Charles de Gaulle', 'street2': 'PA des Moulinets- Bat A', 'zip': '44800', 'city': 'Saint Herblain', 'country_id': {'name': False, 'code': False}, 'partner_latitude': 0, 'partner_longitude': 0, 'email': 'antoine@teremis.fr', 'phone': '0750552605', 'mobile': False, 'is_company': False, 'color': 0, 'partner_share': False, 'commercial_partner_id': {'name': False, 'phone': False, 'email': False, 'street': False, 'city': False, 'zip': False, 'country_id': {'name': False, 'code': False}, 'is_company': False, 'company_name': False}, 'commercial_company_name': False, 'company_name': False, 'signup_type': False, 'invoice_warn_msg': False, 'supplier_rank': 0, 'customer_rank': 0, 'siret': False, 'sale_warn_msg': False}
+            res = completion_id.create_completion(prompt=prompt)
+            return json.loads(res[0])
 
         res = super(BaseModel, self)._get_record_values_from_content(name, content_type, content)
         if not res and content:
@@ -28,8 +27,7 @@ class BaseModel(models.AbstractModel):
                 _logger.error(err, exc_info=True)
             prompt = ('Create a json dictionary as described in this schema: \n %s to create a %s '
                       'following these data: \n %s') % (empty_dict, self._name, content)
-            # res = completion_id.create_completion(prompt=prompt, response_format='json_object')
-            # return json.loads(res[0])
-            return {'email_normalized': 'antoine@teremis.fr', 'message_bounce': 0, 'name': 'Antoine NEBOUT', 'display_name': 'Antoine NEBOUT', 'parent_id': {'name': False, 'phone': False, 'email': False, 'street': False, 'city': False, 'zip': False, 'country_id': {'name': False, 'code': False}, 'is_company': False, 'company_name': False}, 'ref': False, 'vat': False, 'company_registry': False, 'website': False, 'comment': False, 'active': False, 'employee': False, 'function': False, 'street': '16d Charles de Gaulle', 'street2': 'PA des Moulinets- Bat A', 'zip': '44800', 'city': 'Saint Herblain', 'country_id': {'name': False, 'code': False}, 'partner_latitude': 0, 'partner_longitude': 0, 'email': 'antoine@teremis.fr', 'phone': '0750552605', 'mobile': False, 'is_company': False, 'color': 0, 'partner_share': False, 'commercial_partner_id': {'name': False, 'phone': False, 'email': False, 'street': False, 'city': False, 'zip': False, 'country_id': {'name': False, 'code': False}, 'is_company': False, 'company_name': False}, 'commercial_company_name': False, 'company_name': False, 'signup_type': False, 'invoice_warn_msg': False, 'supplier_rank': 0, 'customer_rank': 0, 'siret': False, 'sale_warn_msg': False}
+            res = completion_id.create_completion(prompt=prompt, response_format='json_object')
+            return json.loads(res[0])
 
         return res
